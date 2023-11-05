efc4bc776a7c7d4014663f454872bc34a6eaf290
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-11-05 15:31:00 -0800

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 const init = () => {
-  const inputForm = document.querySelector('form');
-  inputForm.addEventListener('submit', (event) =>{
-      // debugger
+  //grabbing the form
+  const form = document.querySelector('form');
+  //adding an event listener to the form for an event of submit
+  form.addEventListener('submit', (event) =>{
+      //preventing the default behavior
       event.preventDefault();
+      //setting input variable to grab the input 
       const input = document.querySelector("input#searchByID");
-      
-      console.log(input.value);
-      
+
       fetch(`http://localhost:3000/movies/${input.value}`)
       .then((response) => response.json())
       .then((data) => {
-        debugger 
+         
         const title = document.querySelector("section#movieDetails h4")
         const summary = document.querySelector("section#movieDetails p")
         
         title.innerText = data.title;
         summary.innerText = data.summary;
         
-        inputForm.reset()
+        form.reset()
     })
   })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
 
 
 
